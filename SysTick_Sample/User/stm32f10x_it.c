1952e4b97f155ff1394a835ee0509748d5c6e14b
letheascetic <yizhifight@163.com>
2020-01-19 15:21:42 +0800

    update SysTick_Sample, add clean.bat, add bsp_systick.h .c file, not tested.

--- SysTick_Sample/User/stm32f10x_it.c
+++ SysTick_Sample/User/stm32f10x_it.c
@@ -24,6 +24,8 @@
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x_it.h"
 
+extern void TimingDelay_Decrement(void);
+
 /** @addtogroup STM32F10x_StdPeriph_Template
   * @{
   */
@@ -134,6 +136,7 @@ void PendSV_Handler(void)
   */
 void SysTick_Handler(void)
 {
+	TimingDelay_Decrement();
 }
 
 /******************************************************************************/
